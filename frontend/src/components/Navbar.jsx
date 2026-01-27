9827610c813526e9ee89f5559f225c3f4bc090ce
23yuvi23 <yogeshpaliwal9493@gmail.com>
2026-01-27 07:24:52 +0530

    custom css in tailwind

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,21 +9,21 @@ const Navbar = () => {
       <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink>
           <li className="py-1">HOME</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
         <NavLink>
           <li className="py-1">ALL DOCTORS</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
 
         <NavLink>
           <li className="py-1">ABOUT</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
 
         <NavLink>
           <li className="py-1">CONTACT</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto"/>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden"/>
         </NavLink>
       </ul>
 
