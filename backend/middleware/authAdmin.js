511c2feece87bb7c543a14354ff7fda6a2b2d0f4
23yuvi23 <yogeshpaliwal9493@gmail.com>
2026-01-30 10:23:13 +0530

    authAdmin added and adminroute adminController on process

--- backend/middleware/authAdmin.js
+++ backend/middleware/authAdmin.js
@@ -9,7 +9,7 @@ const authAdmin = async (req,res,next)=>{
             return res.json({success:false,message:"Not Authorized Login again"})
         }
         // decode token if available
-        const token_decode = jwt.verify(atoken,process.nextTick.JWT_SECRET)
+        const token_decode = jwt.verify(atoken,process.env.JWT_SECRET)
 
         // check if it is same as email+password
         if (token_decode!== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,17 +1,13 @@
-
-
 import express from 'express'
 import { addDoctor ,loginAdmin } from "../controllers/adminController.js";
-
-
-
 import upload from '../middleware/multer.js'
+import authAdmin from '../middleware/authAdmin.js';
 
 // create router instance 
 const adminRouter = express.Router()
 
-// post endpoint
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor)
+// post endpoint authadmin middleware added
+adminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor)
 
 adminRouter.post('/login',loginAdmin)
 
